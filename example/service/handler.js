a5f85a73847ddfd30cd68fec58c79ac5ce95cd1b
Pedro Luis Guerrero Olivares <53013491+pedroskiplgo@users.noreply.github.com>
2021-06-25 14:33:53 -0400

    Update handler.js

--- example/service/handler.js
+++ example/service/handler.js
@@ -1,4 +1,9 @@
 'use strict';
+//hi i go to do changes in this proyect XD
+
+var num = 2-2;
+console.log(num)
+//already XD................
 
 module.exports.hello = (event, context, callback) => {
   process.stdout.write(event.Records[0].EventSource);
